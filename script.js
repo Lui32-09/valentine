1da1503c6b3891b8b0a0a2f1d3dc03041970d8f8
Lui <98110560+Lui32-09@users.noreply.github.com>
2026-02-01 22:13:26 +0100

    Update message text with newline character

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function showMessage(response) {
 
     // Update text content, show message, and change image source to "dance.gif"
     const yesMessage = document.getElementById("question");
-    yesMessage.textContent = "HIHI:3 LESGOOO 🥳 see you on the 14th 😘😘";
+    yesMessage.textContent = "HIHI:3 LESGOOO 🥳 \n see you on the 14th 😘😘";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
     document.getElementsByClassName("image")[0].src = "images/dance.gif";
